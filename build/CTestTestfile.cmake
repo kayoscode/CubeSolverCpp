8035b9cda616e27c7159ddd891cec7955501dbc4
Bryceyoung <bryce@Bryceyoungs-MacBook-Pro.local>
2025-10-07 19:06:48 -0600

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bryce/Desktop/c/Cube solver
+# Build directory: /Users/bryce/Desktop/c/Cube solver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
